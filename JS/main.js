4c775c6573175c53fc108beb170bf2ac88eee3d4
Oliver Gálvez <oliverglpz@192.168.1.2>
2024-09-17 07:20:37 -0700

    Ajustes del MP

---
+++ JS/main.js
@@ -0,0 +1,42 @@
+const toggleButton = document.querySelector('#toggleModal');
+const modal = document.querySelector('#modal');
+const closeButton = document.querySelector('#closeModal');
+
+const modalDesk = document.querySelector('#modalDesk');
+const closeModal = document.querySelector('#closeDesk');
+const toggleDesk = document.querySelector('#toggleModalDesk');
+
+const email = document.querySelector('#email');
+const emailD = document.querySelector('#emailD');
+const errorMessage = document.querySelector('.error_message')
+
+toggleButton.addEventListener('click', toggleModal);
+closeButton.addEventListener('click', toggleModal);
+
+
+emailValidation.addEventListener('input', validacion)
+toggleDesk.addEventListener('click', toggleModalDesk);
+closeModal.addEventListener('click', toggleModalDesk);
+
+
+function toggleModal() {
+    modal.classList.toggle('hidden')
+}
+
+function toggleModalDesk() {
+    modalDesk.classList.toggle('hidden2')
+}
+
+function validacion() {
+    let ev = email.value;
+    let slicedEmail = ev.split('@');
+
+    if (ev.includes('@') && slicedEmail.length <= 2 && slicedEmail[1].includes('.')){
+        email.classList.remove('este correo no es valido')
+    } else{
+        email.classList.add('este correo no es valido')
+    }
+}
+
+
+
