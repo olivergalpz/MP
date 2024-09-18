bc9f726deba76939a7a8ca85ff958da310e1e888
Oliver Gálvez <oliverglpz@192.168.1.2>
2024-09-17 20:24:05 -0700

    Final commit

--- JS/main.js
+++ JS/main.js
@@ -9,34 +9,49 @@ const toggleDesk = document.querySelector('#toggleModalDesk');
 const email = document.querySelector('#email');
 const emailD = document.querySelector('#emailD');
 const errorMessage = document.querySelector('.error_message')
+const errorMessageDesk = document.querySelector('.error_message_desk')
 
-toggleButton.addEventListener('click', toggleModal);
+
+
 closeButton.addEventListener('click', toggleModal);
-
-
-emailValidation.addEventListener('input', validacion)
-toggleDesk.addEventListener('click', toggleModalDesk);
-closeModal.addEventListener('click', toggleModalDesk);
-
+toggleButton.addEventListener('click', manejarEnvíos);
+function manejarEnvíos() {
+ 
+    let correo = email.value
+    let slicedEmail = correo.split('@');
+    if (correo.includes('@') && slicedEmail.length <= 2 && slicedEmail[1].includes('.')) {
+        toggleModal()
+        email.classList.remove('invalid')
+    }else{
+        errorMessage.textContent = ('Correo no valido')
+        email.classList.add('invalid')
+    }
+}
 
 function toggleModal() {
     modal.classList.toggle('hidden')
+}
+
+
+closeModal.addEventListener('click', toggleModalDesk);
+toggleDesk.addEventListener('click', manejarEnvíosDesk);
+function manejarEnvíosDesk() {
+ 
+    let correoDesk = emailD.value
+    let slicedEmailDesk = correoDesk.split('@');
+    if (correoDesk.includes('@') && slicedEmailDesk.length <= 2 && slicedEmailDesk[1].includes('.')) {
+        toggleModalDesk()
+        emailD.classList.remove('invalid')
+    }else{
+        errorMessageDesk.textContent = ('Correo no valido')
+        emailD.classList.add('invalid')
+    }
 }
 
 function toggleModalDesk() {
     modalDesk.classList.toggle('hidden2')
 }
 
-function validacion() {
-    let ev = email.value;
-    let slicedEmail = ev.split('@');
-
-    if (ev.includes('@') && slicedEmail.length <= 2 && slicedEmail[1].includes('.')){
-        email.textContent.remove('este correo no es valido')
-    } else{
-        email.textContent.add('este correo no es valido')
-    }
-}
 
 
 
